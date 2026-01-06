8b89e5c4b369b67b566117cb008ad478c309968d
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2026-01-06 17:01:49 +0300

    Added Mapping Classes

---
+++ SignalRApi/Mapping/BookingMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.AboutDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Mapping
+{
+    public class BookingMapping : Profile
+    {
+        public BookingMapping()
+        {
+            CreateMap<Booking, ResultAboutDto>().ReverseMap();
+            CreateMap<Booking, CreateAboutDto>().ReverseMap();
+            CreateMap<Booking, UpdateAboutDto>().ReverseMap();
+            CreateMap<Booking, GetAboutDto>().ReverseMap();
+        }
+    }
+}
