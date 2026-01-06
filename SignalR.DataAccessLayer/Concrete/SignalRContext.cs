5f111f17deda922369c5f9e12624c28278514f52
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2026-01-06 14:22:50 +0300

    Added database connection and DbSet definitions to context class

--- SignalR.DataAccessLayer/Concrete/SignalRContext.cs
+++ SignalR.DataAccessLayer/Concrete/SignalRContext.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SignalR.EntityLayer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SignalR.DataAccessLayer.Concrete
 {
-    public class SignalRContext
+    public class SignalRContext :DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=ERTAN; initial Catalog=SignalRDb; integrated security=true;");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
