1bbb348ffad0a65f95d141d5ea06d5dea0926e39
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2026-01-06 14:56:09 +0300

    Added Generic  And EF Classes

---
+++ SignalR.DataAccessLayer/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,17 @@
+﻿using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignalR.DataAccessLayer.EntityFramework
+{
+    public class EfCategoryDal : GenericRepository<Category>, ICategoryDal
+    {
+        public EfCategoryDal(SignalRContext context) : base(context)
+        {
+        }
+    }
+}
