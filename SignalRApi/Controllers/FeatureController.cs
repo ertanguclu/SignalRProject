296509a61a489fc4d750715b055e595c6d9b65cb
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2026-01-06 21:28:25 +0300

    Added Endpoints to Feature And Product Controller

---
+++ SignalRApi/Controllers/FeatureController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.FeatureDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeatureController : ControllerBase
+    {
+        private readonly IFeatureService _featureService;
+        private readonly IMapper _mapper;
+
+        public FeatureController(IMapper mapper, IFeatureService featureService)
+        {
+            _mapper = mapper;
+            _featureService = featureService;
+        }
+        [HttpGet]
+        public IActionResult FeatureList()
+        {
+            var values = _mapper.Map<List<ResultFeatureDto>>(_featureService.TGetListAll());
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateFeature(CreateFeatureDto createFeatureDto)
+        {
+            var value = _mapper.Map<Feature>(createFeatureDto);
+            _featureService.TAdd(value);
+            return Ok("Öne Çıkan Bilgisi Eklendi.");
+        }
+        [HttpDelete]
+        public IActionResult DeleteFeature(int id)
+        {
+            var values = _featureService.TGetById(id);
+            _featureService.TDelete(values);
+            return Ok("Öne Çıkan Bilgisi Silindi.");
+        }
+        [HttpPut]
+        public IActionResult UpdateFeature(UpdateFeatureDto updateFeatureDto)
+        {
+            var value = _mapper.Map<Feature>(updateFeatureDto);
+            _featureService.TUpdate(value);
+            return Ok("Öne Çıkan Alan Bilgisi Güncellendi.");
+        }
+        [HttpGet("GetFeature")]
+        public IActionResult GetFeature(int id)
+        {
+            var value = _mapper.Map<GetFeatureDto>(_featureService.TGetById(id));
+            return Ok(value);
+        }
+    }
+}
---
+++ SignalRApi/Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.ProductDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+
+        public ProductController(IProductService productService, IMapper mapper)
+        {
+            _productService = productService;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult ProductList()
+        {
+            var values = _mapper.Map<List<ResultProductDto>>(_productService.TGetListAll());
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateProduct(CreateProductDto createProductDto)
+        {
+            var value = _mapper.Map<Product>(createProductDto);
+            _productService.TAdd(value);
+            return Ok("Ürün Bilgisi Eklendi.");
+        }
+        [HttpDelete]
+        public IActionResult DeleteProduct(int id)
+        {
+            var values = _productService.TGetById(id);
+            _productService.TDelete(values);
+            return Ok("Ürün Bilgisi Silindi.");
+        }
+        [HttpPut]
+        public IActionResult UpdateProduct(UpdateProductDto updateProductDto)
+        {
+            var value = _mapper.Map<Product>(updateProductDto);
+            _productService.TUpdate(value);
+            return Ok("Ürün Bilgisi Güncellendi.");
+        }
+        [HttpGet("GetProduct")]
+        public IActionResult GetProduct(int id)
+        {
+            var value = _mapper.Map<GetProductDto>(_productService.TGetById(id));
+            return Ok(value);
+        }
+    }
+}
