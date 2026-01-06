dfdf4f8e42c7dd868163db782a9a78e4f13e5282
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2026-01-06 13:59:28 +0300

    Add project files.

---
+++ SignalR.EntityLayer/Entities/SocialMedia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignalR.EntityLayer.Entities
+{
+    public class SocialMedia
+    {
+        public int SocialMediaID { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Icon { get; set; }
+    }
+}
