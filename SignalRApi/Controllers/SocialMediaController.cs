47388a1ed140b6996a7ac205ec10599758f315aa
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2026-01-06 21:40:22 +0300

    Added Endpoints to SocialMedia And Testimonial Controller

---
+++ SignalRApi/Controllers/SocialMediaController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.SocialMediaDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SocialMediaController : ControllerBase
+    {
+        private readonly ISocialMediaService _socialMediaService;
+        private readonly IMapper _mapper;
+
+        public SocialMediaController(IMapper mapper, ISocialMediaService socialMediaService)
+        {
+            _mapper = mapper;
+            _socialMediaService = socialMediaService;
+        }
+        [HttpGet]
+        public IActionResult SocialMediaList()
+        {
+            var values = _mapper.Map<List<ResultSocialMediaDto>>(_socialMediaService.TGetListAll());
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateSocialMedia(CreateSocialMediaDto createSocialMediaDto)
+        {
+            var values = _mapper.Map<SocialMedia>(createSocialMediaDto);
+            _socialMediaService.TAdd(values);
+            return Ok("Sosyal Medya Bilgisi Eklendi");
+        }
+        [HttpDelete]
+        public IActionResult DeleteSocialMedia(int id)
+        {
+            var values = _socialMediaService.TGetById(id);
+            _socialMediaService.TDelete(values);
+            return Ok("Sosyal Medya Bilgisi Silindi");
+        }
+        [HttpPut]
+        public IActionResult UpdateSocialMedia(UpdateSocialMediaDto updateSocialMediaDto)
+        {
+            var values = _mapper.Map<SocialMedia>(updateSocialMediaDto);
+            _socialMediaService.TUpdate(values);
+            return Ok("Sosyal Medya Bilgisi Güncellendi");
+        }
+        [HttpGet("GetSocialMedia")]
+        public IActionResult GetSocialMedia(int id)
+        {
+            var values = _mapper.Map<GetSocialMediaDto>(_socialMediaService.TGetById(id));
+            return Ok(values);
+        }
+    }
+}
---
+++ SignalRApi/Controllers/TestimonialController.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.TestimonialDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialController : ControllerBase
+    {
+        private readonly ITestimonialService _testimonialService;
+        private readonly IMapper _mapper;
+
+        public TestimonialController(IMapper mapper, ITestimonialService testimonialService)
+        {
+            _mapper = mapper;
+            _testimonialService = testimonialService;
+        }
+        [HttpGet]
+        public IActionResult TestimonialList()
+        {
+            var values = _mapper.Map<List<ResultTestimonialDto>>(_testimonialService.TGetListAll());
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateTestimonial(CreateTestimonialDto createTestimonialDto)
+        {
+            var values = _mapper.Map<Testimonial>(createTestimonialDto);
+            _testimonialService.TAdd(values);
+            return Ok("Müşteri Yorum Bilgisi Eklendi");
+        }
+        [HttpDelete]
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var values = _testimonialService.TGetById(id);
+            _testimonialService.TDelete(values);
+            return Ok("Müşteri Yorum Bilgisi Silindi");
+
+        }
+        [HttpPut]
+        public IActionResult UpdateTestimonial(UpdateTestimonialDto updateTestimonialDto)
+        {
+            var values = _mapper.Map<Testimonial>(updateTestimonialDto);
+            _testimonialService.TUpdate(values);
+            return Ok("Müşteri Yorum Bilgisi Güncellendi");
+        }
+        [HttpGet("GetTestimonial")]
+        public IActionResult GetTestimonial(int id)
+        {
+            var values = _mapper.Map<GetTestimonialDto>(_testimonialService.TGetById(id));
+            return Ok(values);
+        }
+    }
+}
