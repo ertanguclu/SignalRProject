bc0b1a384c27154ade6754544637407585e1af08
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2026-01-21 21:07:37 +0300

    Added Testimonial To HomePage

--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultBookAtTableComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultBookAtTableComponentPartial.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.AboutDtos;
+using SignalRWebUI.Dtos.BookingDtos;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents
 {
     public class _DefaultBookAtTableComponentPartial :ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultBookAtTableComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7161/api/Booking");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultBookingDto>>(jsonData);
+            return View(values);
         }
     }
 }
