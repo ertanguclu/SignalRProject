ab3c82649f991b7c097de2d1938e499c799a6413
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2026-01-27 15:48:09 +0300

    Added Endpoint To Basket Controller

---
+++ SignalRApi/Controllers/BasketController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketService _basketService;
+
+        public BasketController(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+        [HttpGet]
+        public IActionResult GetBasketByMenuTableID(int id)
+        {
+            var values = _basketService.TGetBasketByTableNumber(id);
+            return Ok(values);
+        }
+    }
+}
