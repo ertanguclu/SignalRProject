06b65fbcbe9a2b84f83fce6bf24ecef58c4dc046
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2026-01-28 20:34:45 +0300

    added create and updating view for notification

--- SignalRWebUI/Dtos/NotificationDtos/CreateNotificationDto.cs
+++ SignalRWebUI/Dtos/NotificationDtos/CreateNotificationDto.cs
@@ -9,7 +9,6 @@ namespace SignalRWebUI.Dtos.NotificationDtos
         public string Type { get; set; }
         public string Icon { get; set; }
         public string Description { get; set; }
-        public DateTime Date { get; set; }
         public bool Status { get; set; }
     }
 }
