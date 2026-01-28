edca2cd0b62578bcc67f09c2bdd935cd211563f6
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2026-01-28 17:26:11 +0300

    Added Notification entity

---
+++ SignalR.EntityLayer/Entities/Notification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignalR.EntityLayer.Entities
+{
+    public class Notification
+    {
+        public int NotificationID { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public bool Status { get; set; }
+    }
+}
