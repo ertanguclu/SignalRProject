a8c976c94848ddaf357dab20b40cd5cee4071a7c
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2026-01-28 19:32:38 +0300

    Added Dto For Notification Entity

---
+++ SignalR.DtoLayer/NotificationDto/CreateNotificationDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignalR.DtoLayer.NotificationDto
+{
+    public class CreateNotificationDto
+    {
+        public string Type { get; set; }
+        public string Icon { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public bool Status { get; set; }
+    }
+}
