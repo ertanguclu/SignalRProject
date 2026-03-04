cd3ba50f94ba29119332b94776bb4cb4fd155489
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2026-03-04 14:04:23 +0300

    Added MenuTableDtos

---
+++ SignalR.DtoLayer/MenuTableDto/CreateMenuTableDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignalR.DtoLayer.MenuTableDto
+{
+    public class CreateMenuTableDto
+    {
+        public string Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
--- SignalRApi/Controllers/MenuTableController.cs
+++ SignalRApi/Controllers/MenuTableController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.AboutDto;
+using SignalR.EntityLayer.Entities;
 
 namespace SignalRApi.Controllers
 {
@@ -19,6 +21,39 @@ namespace SignalRApi.Controllers
         {
             var menuTableCount = _menuTableService.TMenuTableCount();
             return Ok(menuTableCount);
+        }
+        [HttpGet]
+        public IActionResult MenuTableList()
+        {
+            var values = _mapper.Map<List<ResultAboutDto>>(_aboutService.TGetListAll());
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateAbout(CreateAboutDto createAboutDto)
+        {
+            var value = _mapper.Map<About>(createAboutDto);
+            _aboutService.TAdd(value);
+            return Ok("Hakkında Kısmı Başarılı Bir Şekilde Eklendi.");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteAbout(int id)
+        {
+            var values = _aboutService.TGetById(id);
+            _aboutService.TDelete(values);
+            return Ok("Hakkında Kısmı Başarılı Bir Şekilde Silindi.");
+        }
+        [HttpPut]
+        public IActionResult UpdateAbout(UpdateAboutDto updateAboutDto)
+        {
+            var value = _mapper.Map<About>(updateAboutDto);
+            _aboutService.TUpdate(value);
+            return Ok("Hakkında Kısmı Başarılı Bir Şekilde Güncellendi.");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetAbout(int id)
+        {
+            var value = _mapper.Map<GetAboutDto>(_aboutService.TGetById(id));
+            return Ok(value);
         }
     }
 }
