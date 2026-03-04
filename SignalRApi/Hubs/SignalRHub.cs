36151bb3babd037df248d239b706095220c62ee2
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2026-03-04 16:48:49 +0300

    Added table availability indicator (empty/occupied) to the view

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -96,5 +96,10 @@ namespace SignalRApi.Hubs
             var notificationListByFalse = _notificationService.TGetAllNotificationByFalse();
             await Clients.All.SendAsync("ReceiveNotificationListByFalse", notificationListByFalse);
         }
+        public async Task GetMenuTableStatus() 
+        {
+            var values = _menuTableService.TGetListAll();
+            await Clients.All.SendAsync("ReceiveMenuTableStatus", values);
+        }
     }
 }
--- SignalRWebUI/Controllers/MenuTableController.cs
+++ SignalRWebUI/Controllers/MenuTableController.cs
@@ -34,7 +34,7 @@ namespace SignalRWebUI.Controllers
             return View();
         }
         [HttpPost]
-        public async Task<IActionResult> CreateMenuTable(CreateMenuTableDto  createMenuTableDto)
+        public async Task<IActionResult> CreateMenuTable(CreateMenuTableDto createMenuTableDto)
         {
             createMenuTableDto.Status = false;
             var client = _httpClientFactory.CreateClient();
@@ -81,6 +81,19 @@ namespace SignalRWebUI.Controllers
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> TableListByStatus()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7161/api/MenuTable");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultMenuTableDto>>(jsonData);
+                return View(values);
             }
             return View();
         }
