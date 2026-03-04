dad4d5ce045c8cced43832dfc3abe36ba980bd52
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2026-03-04 17:53:45 +0300

    Added chat.js

---
+++ SignalRWebUI/wwwroot/js/chat.js
@@ -0,0 +1,17 @@
+var connection = new signalR.HubConnectionBuilder().withUrl("https://localhost:7161/SignalRHub").build();
+document.getElementById("sendbutton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var currentTime = new Date();
+    var currentHour = currentTime.getHours();
+    var currentMinute = currentTime.getMinutes();
+
+    var li = document.createElement("li");
+    var span = document.createElement("span");
+    span.style.fontWeight = "bold";
+    span.textContent = user;
+    li.appendChild(span);
+    li.innerHTML += ` :${message} - ${currentHour}:${currentMinute}`;
+    document.getElementById("messageslist").appendChild(li);
+
+});
