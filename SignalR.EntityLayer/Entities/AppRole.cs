35a3c80d6d36330036e9734b437a98ddb2dd25eb
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2026-03-04 20:03:17 +0300

    Added User And Role Entities

---
+++ SignalR.EntityLayer/Entities/AppRole.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignalR.EntityLayer.Entities
+{
+    public class AppRole : IdentityRole<int>
+    {
+
+    }
+}
---
+++ SignalR.EntityLayer/Entities/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignalR.EntityLayer.Entities
+{
+    public class AppUser : IdentityUser<int>
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
