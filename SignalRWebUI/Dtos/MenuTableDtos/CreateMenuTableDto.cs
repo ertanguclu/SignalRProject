20316f449a704bdbd6e403b904a2f2ac74177af0
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2026-03-04 14:33:05 +0300

    Added Menutable Controller And Dtos

---
+++ SignalRWebUI/Dtos/MenuTableDtos/CreateMenuTableDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignalRWebUI.Dtos.MenuTableDtos
+{
+    public class CreateMenuTableDto
+    {
+        public string Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ SignalRWebUI/Dtos/MenuTableDtos/UpdateMenuTableDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignalRWebUI.Dtos.MenuTableDtos
+{
+    public class UpdateMenuTableDto
+    {
+        public int MenuTableID { get; set; }
+        public string Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
