f968b617ceb026daa7e8b521515809536e4aedc8
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2026-03-05 16:10:03 +0300

    Added approve and cancel buttons with endpoints

--- SignalRWebUI/Controllers/BookingController.cs
+++ SignalRWebUI/Controllers/BookingController.cs
@@ -82,5 +82,25 @@ namespace SignalRWebUI.Controllers
             }
             return View();
         }
+        public async Task<IActionResult> BookingStatusApproved(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7161/api/Booking/BookingStatusApproved/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public async Task<IActionResult> BookingStatusCancelled(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7161/api/Booking/BookingStatusCancel/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
