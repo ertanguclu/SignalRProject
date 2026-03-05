d66756e699174756666d0dc1630ec1ab9bed35c6
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2026-03-05 15:24:50 +0300

    Added Booking Description

--- SignalRWebUI/Dtos/BookingDtos/CreateBookingDto.cs
+++ SignalRWebUI/Dtos/BookingDtos/CreateBookingDto.cs
@@ -11,5 +11,6 @@ namespace SignalRWebUI.Dtos.BookingDtos
         public string Mail { get; set; }
         public int PersonCount { get; set; }
         public DateTime Date { get; set; }
+        public string Description { get; set; }
     }
 }
--- SignalRWebUI/Dtos/BookingDtos/ResultBookingDto.cs
+++ SignalRWebUI/Dtos/BookingDtos/ResultBookingDto.cs
@@ -12,5 +12,6 @@ namespace SignalRWebUI.Dtos.BookingDtos
         public string Mail { get; set; }
         public int PersonCount { get; set; }
         public DateTime Date { get; set; }
+        public string Description { get; set; }
     }
 }
